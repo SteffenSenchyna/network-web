9402e3225d8a2ffe734b1563bea11e00a5b7e305
SteffenSenchyna <105737839+SteffenSenchyna@users.noreply.github.com>
2023-02-17 21:05:57 -0700

    Deleted .next

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/devices":["static\u002Fchunks\u002Fpages\u002Fdevices.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fdevices"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
